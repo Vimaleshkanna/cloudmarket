343965a3a62a4e95b0a3a968587bf268f905b3db
srini4ai <srinivasanvelu23@gmail.com>
2025-08-09 04:49:26 -0700

    cart, cart history

---
+++ cloudmarket-frontend/src/Components/Pages/Products/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Products() {
+    return (
+ <div className="row">
+ <div className="card col-lg-3">
+  <div className="card-body">
+    <img src="https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_t.png" className="img-fluid object-image"/>
+    <h5 className="card-title">Mens Casual Premium Slim Fit T-Shirts</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <button className="card-link btn btn-primary">Add To Cart</button>
+  </div></div>
+</div>)
+}
